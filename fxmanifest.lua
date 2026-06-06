8fe5587a98542efee454d5b7175d9e9d2592bba0
helnius <jimihelenius@icloud.com>
2026-06-06 15:58:00 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+lua54 'yes'
+author 'helnius' -- Discord: https://discord.gg/RGgqnsxvMv
+description 'helnius interior mapdata'
+version '1.0-release'
+
+replace_level_meta 'gta5'
+
+files {
+    'gta5.meta',
+    'doortuning.ymt',
+    'helnius_map_door_audios_game.dat151.rel',
+    'helnius_map_audios_game.dat151.rel'
+}
+
+data_file 'AUDIO_GAMEDATA' 'helnius_map_door_audios_game.dat'
+data_file 'AUDIO_GAMEDATA' 'helnius_map_audios_game.dat'
