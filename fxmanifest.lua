1910947a8a45bf7b8e202d9dd523bbb886644db7
helnius <jimihelenius@icloud.com>
2026-07-20 16:51:35 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,9 +11,8 @@ replace_level_meta 'gta5'
 files {
     'gta5.meta',
     'doortuning.ymt',
-    'helnius_map_door_audios_game.dat151.rel',
-    'helnius_map_audios_game.dat151.rel'
+    'audio/helnius_map_audios_game.dat151.rel'
 }
 
-data_file 'AUDIO_GAMEDATA' 'helnius_map_door_audios_game.dat'
-data_file 'AUDIO_GAMEDATA' 'helnius_map_audios_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/helnius_map_audios_game.dat'
+
